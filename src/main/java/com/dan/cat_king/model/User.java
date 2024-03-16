8641173cfaba1ecc477fc4caeceb15b4949e4a5a
GinzaTech <thanhloc150204@gmail.com>
2024-03-16 20:27:41 +0700

    test

--- src/main/java/com/dan/cat_king/model/User.java
+++ src/main/java/com/dan/cat_king/model/User.java
@@ -13,8 +13,6 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private String address;
-    private String phoneNumber;
     private String email;
     private String password;
 }
